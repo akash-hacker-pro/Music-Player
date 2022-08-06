b71de67a2d481a63e8cb6a09d7307978ed7725fb
Akash R <67360645+akash-hacker-pro@users.noreply.github.com>
2022-08-06 22:59:32 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,71 @@
+let allMusic = [
+  {
+    name: "Bae",
+    artist: "Anirudh Ravichander",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Idhayathai Oru Nodi",
+    artist: "Yuvan Shankar Raja",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kaadhal En Kaviye",
+    artist: "Sid Sriram",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Marudaani",
+    artist: "Unknown",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Mayakirriye",
+    artist: "Anirudh Ravichander",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Naan Pizhai",
+    artist: "Anirudh Ravichander",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Oru Devathai",
+    artist: "Yuvan Shankar Raja",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Siragugal",
+    artist: "Yuvan_Shankar_Raja",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Vilambara Idaivel",
+    artist: "Hiphop Tamizha",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Yathe Yathe",
+    artist: "GV Prakash Kumar",
+    img: "music-10",
+    src: "music-10"
+  },
+ 
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
