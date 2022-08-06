a42abbe66f1943ca529f85d0c088df6d6d71d806
Akash R <67360645+akash-hacker-pro@users.noreply.github.com>
2022-08-06 21:05:09 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,82 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Bae",
+    artist: " Anirudh Ravichander",
+    img: "Bae-From-Don--Tamil-2022-20220203163123-500x500",
+    src: "Bae - Anirudh Ravichander, Adithya RK"
+  },
+  {
+    name: "Idhayathai Oru Nodi",
+    artist: "Yuvan Shankar Raja",
+    img: "Semma-Botha-Aagathey-Tamil-2017-20210530043830-500x500",
+    src: "Idhayathai Oru Nodi - Yuvan Shankar Raja"
+  },
+  {
+    name: "Kaadhal En Kaviye",
+    artist: "Sid Sriram",
+    img: "Salmon-3D-Tamil-2021-20210326032707-500x500",
+    src: "Kaadhal En Kaviye - Sid Sriram"
+  },
+  {
+    name: "Marudaani",
+    artist: "Unknown",
+    img: "Sakkarakatti-Tamil-2008-500x500",
+    src: "Marudaani - Unknown"
+  },
+  {
+    name: "Mayakirriye",
+    artist: "Anirudh Ravichander",
+    img: "Mayakirriye-Tamil-2022-20220208112058-500x500",
+    src: "Mayakirriye - Anirudh Ravichander"
+  },
+  {
+    name: "Naan Pizhai",
+    artist: "Anirudh Ravichander",
+    img: "Kaathuvaakula-Rendu-Kaadhal-Original-Motion-Picture-Soundtrack-Tamil-2022-20220428131043-500x500",
+    src: "Naan Pizhai - Anirudh Ravichander, Ravi G, Shashaa Tirupati"
+  },
+  {
+    name: "Oru Devathai",
+    artist: "Yuvan Shankar Raja",
+    img: "Vaamanan-Tamil-2009-20190629140956-500x500",
+    src: "Oru Devathai - Yuvan Shankar Raja, Roopkumar Rathod"
+  },
+  {
+    name: "Siragugal",
+    artist: "Yuvan_Shankar_Raja",
+    img: "siragugal-vanthathu-song-lyrics",
+    src: "Sarvam___Siragugal_-_Lyric_Video___Arya___Trisha___Vishnuvardhan___Yuvan_Shankar_Raja___Ayngaran(256k)"
+  },
+  {
+    name: "Vilambara Idaivel",
+    artist: "Hiphop Tamizha",
+    img: "Imaikkaa-Nodigal-Tamil-2017-20180908-500x500",
+    src: "Vilambara Idaiveli - Christopher Stanley, Sudharshan Ashok, Srinisha Jayaseelan, Hiphop Tamizha"
+  },
+  {
+    name: "Yathe Yathe",
+    artist: "GV Prakash Kumar",
+    img: "Aadukalam-Tamil-2010-20190731133223-500x500",
+    src: "Yathe Yathe - GV Prakash Kumar"
+  },
+ 
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
